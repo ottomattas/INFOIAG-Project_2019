bdc3f4001784ee44c45687fec4a6cfa8fcd8a340
Unknown <pippo.libardi@gmail.com>
2019-10-10 16:34:31 +0200

    Added Skills
    Added skills and relations between them

--- wine_query.py
+++ wine_query.py
@@ -9,53 +9,60 @@ sync_reasoner()
 # Wine and hasBody value Medium
 # onto.Wine & onto.hasBody.value(Medium)
 try:
-    white_wines = onto.search(is_a = onto.WhiteWine)
+    white_wines = onto.search(is_a=onto.WhiteWine)
     for w in white_wines:
         print(w)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
-    fullBody_wines = onto.search(is_a = onto.Wine & onto.WineBody.value(Full))
+    fullBody_wines = onto.search(is_a=onto.Wine & onto.WineBody.value(Full))
     for w in fullBody_wines:
         print(w)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
-    red_wines = onto.search(type = onto.Wine & onto.Wine.hasColor.value(Red))
+    red_wines = onto.search(type=onto.Wine & onto.Wine.hasColor.value(Red))
     for w in red_wines:
         print(w)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
     print(onto.hasWineDescriptor.get_relations())
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
-    fullBody_wines = onto.search(is_a = onto.Wine, hasBody = onto.search(is_a = onto.WineBody))
+    fullBody_wines = onto.search(is_a=onto.Wine, hasBody=onto.search(is_a=onto.WineBody))
     for w in fullBody_wines:
         print(w)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
-    fullBody_wines = onto.search(hasBody = "*Full")
+    fullBody_wines = onto.search(hasBody="*Full")
     for w in fullBody_wines:
         print(w)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
 
 try:
     for i in onto.RedWine.instances(): print(i)
-except Exception as e: print(e)
+except Exception as e:
+    print(e)
 
 print("-" * 60)
