e539203873cd19a490b08bb5e11d1f4ba6321714
Filippo Libardi <pippo.libardi@gmail.com>
2019-10-22 12:04:07 +0200

    First stages of Agent Architecture

--- agent.py
+++ agent.py
@@ -1,42 +1,63 @@
 import json
 import os
-import pandas as pd
-from owlready2 import *
 import warnings
 
+import pandas as pd
+from owlready2 import *
+
 warnings.filterwarnings("ignore")
-print("\033c")
 
-ONTOLOGY_FILE   = "./model.owl"
-DATA_FILE       = "./student_data.json"
+ONTOLOGY_FILE = "./ultimate_ontology.owl"
+DATA_FILE = "./student_data.json"
 
 
-class agent():
+class Agent():
 
-    def __init__(self, ontology, data):
-        self.ontology   = ontology   
-        
+    def __init__(self, ontology, data, s_index):
+        self.ontology = ontology
+
         with open(data) as json_data:
             self.student_data = json.load(json_data)
 
-    def extract_courses_taken(self):
+        self.student_obj = self.ontology.search(studentID=self.student_data[s_index]["id"])[0]
 
-        student = self.ontology.Person.Student.id(self.data.id)
-        print(student)
+    def extract_courses_taken(self):
+        taken_courses = self.student_obj.hasTaken
+        return taken_courses
+
+    def extract_possible_courses(self):
+        can_take = self.student_obj.canTake
+        return can_take
+
+    def extract_basic_courses(self):
+        basic_courses = self.ontology.BasicCourse
+        return basic_courses
 
     def preliminary_courses(self, courses_has_taken):
         pass
+
     def make_rankings(self):
         pass
+
     def apply_filter(self):
         pass
-    def built_combinations(self, possible_courses):
-        pass
+
+    def build_combinations(self):
+        taken_course = self.extract_courses_taken()
+        possible_course = self.extract_possible_courses()
+        basic_course = self.extract_basic_courses().instances()
+
+        comb = [c for c in possible_course if c not in taken_course]
+
+
+
+        print(comb)
+
     def check_consistency(self):
         pass
 
-def main():
 
+def main():
     # path to the wine ontology
     onto = get_ontology(ONTOLOGY_FILE)
     onto.load()
@@ -44,8 +65,9 @@ def main():
     with onto:
         sync_reasoner(infer_property_values=True)
 
-        new_agent = agent(onto, DATA_FILE)
-        new_agent.extract_courses_taken()
+        new_agent = Agent(onto, DATA_FILE, 0)
+
+        new_agent.build_combinations()
 
 
 if __name__ == "__main__":
