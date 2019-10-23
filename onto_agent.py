553d28b70a5199961505d8b66f42b7a395f879e2
sorindragan <sorin.dragan27@gmail.com>
2019-10-23 22:21:25 +0200

    finished baseline ranking implementation

--- onto_agent.py
+++ onto_agent.py
@@ -13,7 +13,7 @@ ONTOLOGY_FILE   = "./ultimate_ontology.owl"
 DATA_FILE       = "./student_data.json"
 COURSE_THRESHOLD = 5
 
-class agent():
+class Agent():
 
     def __init__(self, ontology, data, s_index):
         self.ontology   = ontology
@@ -22,6 +22,11 @@ class agent():
             self.data = json.load(json_data)
 
         self.student_obj = self.ontology.search(studentID=self.data["id"])[0]
+
+    @staticmethod
+    def print_debug(*param):
+        print(param)
+        print("-" * 50)
 
     def extract_courses_taken(self):
         taken_courses = self.student_obj.hasTaken
@@ -70,34 +75,46 @@ class agent():
         for idx, p in enumerate(importance[1:-1]):
             pref_weight[p] = 2 - (idx + 1) * split
 
-        print("Preferences Weight: ")
-        pprint(pref_weight)
+        Agent.print_debug("Preferences Weight: ", pref_weight)
         return pref_weight
 
+    # checked
+    def get_topics_scores(self, course, pref_topics):
+        topics = course.covers
+        topics = [str(t).split(".")[1] for t in topics]
+        return len(set(topics).intersection(set(pref_topics)))
 
-    def get_topics_scores(self, course, topics):
-        pass
+    # checked
+    def get_skills_scores(self, course, pref_skills):
+        research_met = course.uses
+        skills = research_met[0].improves
+        skills = [str(s).split(".")[1] for s in skills]
+        return len(set(skills).intersection(set(pref_skills)))
 
     def calculate_score(self, packege, pref_w):
         preferences = self.data["preferences"]
         score_per_course = []
         for c in packege:
             course_scores = []
-            course_scores.append(self.get_topics_scores(c, preferences["topics"]))
-            # do the same for each preference (ex skills etc)
+            course_scores.append(pref_w["topics"] * self.get_topics_scores(c, preferences["topics"]))
+            course_scores.append(pref_w["skills"] * self.get_skills_scores(c, preferences["skills"]))
+            # TODO: compute other preferences
+
             score_per_course.append(sum(course_scores))
-
-
 
         return sum(score_per_course)
 
     def rank(self, packages):
-        package_score_dict = {}
+        package_score_list = []
         importance_list = self.data["importance"]
         pref_weight = self.map_to_interval(importance_list)
         for p in packages:
-            package_score_dict[tuple(p)] = self.calculate_score(p, pref_weight)
-        return package_score_dict
+            package_score_list.append((p, self.calculate_score(p, pref_weight)))
+
+        package_score_list.sort(key = lambda t: t[1])
+
+        Agent.print_debug("Best packeges: ", package_score_list[-2:])
+        return package_score_list
 
     def match_preferences(self):
 
@@ -105,14 +122,14 @@ class agent():
         importance_list = self.data["importance"]
         period = preferences["period"]
         courses = self.get_courses_per_period()[period]
+        Agent.print_debug("Courses from period [" + period + "]: ", courses)
         if len(courses) <= COURSE_THRESHOLD:
             packages = self.generate_combinations(courses)
-            print("Generated Packages: ", packages)
+            Agent.print_debug("Generated Packages: ", packages, "Number: ", len(packages))
             ranked_packages = self.rank(packages)
+        # TODO: else: ??
 
-
-        print("Courses from period [" + period + "]: ", courses)
-
+        return ranked_packages[-1]
 
     # checked
     def get_courses_per_period(self):
@@ -162,8 +179,8 @@ def course_planning(agent):
     coursesTaken        = agent.extract_courses_taken()
     all_courses         = agent.get_all_courses()
     most_similar        = agent.get_similar_courses_to(coursesTaken[0], all_courses, 3)
-    print("Taken Courses: ", coursesTaken)
-    print("Most similar courses to: [", coursesTaken[0], "] are the following: ", most_similar)
+    Agent.print_debug("Taken Courses: ", coursesTaken)
+    Agent.print_debug("Most similar courses to: [", coursesTaken[0], "] are the following: ", most_similar)
 
     agent.match_preferences()
 
@@ -178,7 +195,7 @@ def main():
     with onto:
         sync_reasoner(infer_property_values=True)
 
-        onto_agent = agent(onto, DATA_FILE, 0)
+        onto_agent = Agent(onto, DATA_FILE, 0)
         course_planning(onto_agent)
 
 
