2267e9351d47017ae33961b489aa4ca25821f87c
Unknown <pippo.libardi@gmail.com>
2019-10-25 02:39:58 +0200

    Hobby clash and working
    ++> onto_agent.py 
    - similarity_rank method, added print of the after rank courses and removed the scale by size (too biased on suggesting three)
    - Added check hobbies method catching inconsistency for hobbies.
    - General refactoring of imports and variables/methods name.
    - Made several class methods as static as they don't use out of scope variables. We might think about having them as separate functions.
    ++> ultimate_ontology.owl :
    - Added Student1 takes PhilosophyOfAI on Tu.
    ++> student_data.json :
    -Hobby field is not football anymore. Put football to get a clash with PhilosophyOfAI.

--- onto_agent.py
+++ onto_agent.py
@@ -1,68 +1,70 @@
 import json
 import os
-import pandas as pd
-import numpy as np
-from owlready2 import *
-from pprint import pprint
 import warnings
 from itertools import combinations
+from pprint import pprint
+
+import numpy as np
+import pandas as pd
+from owlready2 import *
 
 warnings.filterwarnings("ignore")
 
-ONTOLOGY_FILE   = "./ultimate_ontology.owl"
-DATA_FILE       = "./student_data.json"
+ONTOLOGY_FILE = "./ultimate_ontology.owl"
+DATA_FILE = "./student_data.json"
+
 
 # Observations:
-#TODO: - Remove Bias (2 vs 3 course packages)
-#TODO: - Scaling / Normalization
-#TODO: - Score X - Similarity X- (?? what should we do in case of equality) ???? The INFO GAIN approach is biased back to the firrst question ???? NO
-#TODO: - Performance measure: average score of random/total course over the average score of the filtered/best ones - How much better is our?
-#TODO: - Equal imporance for preferences (same weight)
-#TODO: - Learn new skills preference (only if we have time)
-#TODO: - Translate from pythonic queries to Manchester queries
+# TODO: Remove Bias (2 vs 3 course packages)
 
-class Agent():
+# TODO: Scaling / Normalization
 
-    def __init__(self, data, s_index):
+# TODO: Score X - Similarity X- (?? what should we do in case of equality) ????
+#  The INFO GAIN approach is biased back to the firrst question ???? NO
 
-        self.ontology   = get_ontology(ONTOLOGY_FILE)
+# TODO: Performance measure: average score of random/total
+#  course over the average score of the filtered/best ones - How much better is our?
+
+# TODO: Equal importance for preferences (same weight)
+
+# TODO: Learn new skills preference (only if we have time)
+
+# TODO: Translate from pythonic queries to Manchester queries
+
+class Agent:
+
+    def __init__(self, data):
+
+        self.ontology = get_ontology(ONTOLOGY_FILE)
         self.ontology.load()
         with self.ontology:
             sync_reasoner(infer_property_values=True)
 
         with open(data) as json_data:
-            self.data   = json.load(json_data)
+            self.data = json.load(json_data)
 
-        self.student_obj            = self.ontology.search(studentID=self.data["id"])[0]
-        self.disliked_teachers_obj  = [self.ontology.search(teacherID=teacher_id)[0]
-                                        for teacher_id in self.data["preferences"]["dislikes"]
-                                        ]
-        self.liked_teachers_obj     = [self.ontology.search(teacherID=teacher_id)[0]
-                                        for teacher_id in self.data["preferences"]["likes"]
-                                        ]
-        self.friends_obj            = self.student_obj.hasFriend
-
+        self.student_obj = self.ontology.search(studentID=self.data["id"])[0]
+        self.disliked_teachers_obj = [self.ontology.search(teacherID=teacher_id)[0]
+                                      for teacher_id in self.data["preferences"]["dislikes"]
+                                      ]
+        self.liked_teachers_obj = [self.ontology.search(teacherID=teacher_id)[0]
+                                   for teacher_id in self.data["preferences"]["likes"]
+                                   ]
+        self.friends_obj = self.student_obj.hasFriend
 
     @staticmethod
     def print_debug(*param):
-        print(param)
+        pprint(param)
         print("-" * 50)
 
-    def extract_courses_taken(self):
-        taken_courses = self.student_obj.hasTaken
-        return taken_courses
+    @staticmethod
+    def extract_topics(course_list):
+        return set([topic.name
+                    for topic in list(itertools.chain.from_iterable([c.covers for c in course_list]))
+                    ])
 
-    def extract_possible_courses(self):
-        can_take = self.student_obj.canTake
-        return can_take
-
-    def extract_basic_courses(self):
-        basic_courses = self.ontology.BasicCourse
-        return basic_courses
-
-    # checked
-    def get_similar_courses_to(self, course, all_courses, no):
-
+    @staticmethod
+    def get_similar_courses_to(course, all_courses, no):
         similar_courses = []
         topics = course.covers
         for c in all_courses:
@@ -70,18 +72,19 @@ class Agent():
                 continue
             c_topic = c.covers
             similar = len(set(c_topic).intersection(set(topics)))
-            desimilar = len(set(c_topic).union(set(topics))) - similar
-            similar_courses.append((c, similar/(similar + desimilar)))
+            dissimilar = len(set(c_topic).union(set(topics))) - similar
+            similar_courses.append((c, similar / (similar + dissimilar)))
 
         similar_courses.sort(key=lambda t: t[1])
 
         return [c[0] for c in similar_courses[-no:]]
 
-    # checked
-    def generate_combinations(self, courses):
+    @staticmethod
+    def generate_combinations(courses):
         return list(combinations(courses, 2)) + list(combinations(courses, 3))
 
-    def map_to_interval(self, importance):
+    @staticmethod
+    def map_to_interval(importance):
         pref_weight = {
             "topics": 0,
             "skills": 0,
@@ -98,51 +101,60 @@ class Agent():
         Agent.print_debug("Preferences Weight: ", pref_weight)
         return pref_weight
 
-    # checked
-    def get_topics_scores(self, course, pref_topics):
+    @staticmethod
+    def get_topics_scores(course, pref_topics):
         topics = course.covers
         topics = [t.name for t in topics]
         return len(set(topics).intersection(set(pref_topics)))
 
-    # checked
-    def get_skills_scores(self, course, pref_skills):
+    @staticmethod
+    def get_skills_scores(course, pref_skills):
         research_met = course.uses
         skills = research_met[0].improves
         skills = [s.name for s in skills]
         return len(set(skills).intersection(set(pref_skills)))
 
-    def get_dislikes_scores(self, course, pref_dislike, weight):
+    def extract_courses_taken(self):
+        taken_courses = self.student_obj.hasTaken
+        return taken_courses
+
+    def extract_possible_courses(self):
+        can_take = self.student_obj.canTake
+        return can_take
+
+    def extract_basic_courses(self):
+        basic_courses = self.ontology.BasicCourse
+        return basic_courses
+
+    def get_dislikes_scores(self, course, weight):
         for teacher in self.disliked_teachers_obj:
             if course.name in [c.name for c in teacher.teaches]:
                 # why did we do this shit here?
                 return -weight
         return 1
 
-    def get_likes_scores(self, course, pref_likes, weight):
+    def get_likes_scores(self, course, weight):
         for teacher in self.liked_teachers_obj:
             if course.name in [c.name for c in teacher.teaches]:
                 return weight
         return 1
 
-    def get_friends_scores(self, course, pref_friends, weight):
+    def get_friends_scores(self, course, weight):
         friends_courses = [friend.takes for friend in self.friends_obj]
         courses = [c.name for c in list(itertools.chain.from_iterable(friends_courses))]
         if course.name in courses:
             return weight
         return 1
 
-    def calculate_score(self, packege, pref_w):
+    def calculate_score(self, package, pref_w):
         preferences = self.data["preferences"]
         score_per_course = []
-        for c in packege:
-            course_scores = []
-            course_scores.append(pref_w["topics"] * self.get_topics_scores(c, preferences["topics"]))
-            course_scores.append(pref_w["skills"] * self.get_skills_scores(c, preferences["skills"]))
-            course_scores.append(self.get_dislikes_scores(c, preferences["dislikes"], pref_w["dislikes"]))
-            course_scores.append(self.get_likes_scores(c, preferences["likes"], pref_w["likes"]))
-            course_scores.append(self.get_friends_scores(c, preferences["friends"], pref_w["friends"]))
-
-
+        for c in package:
+            course_scores = [pref_w["topics"] * self.get_topics_scores(c, preferences["topics"]),
+                             pref_w["skills"] * self.get_skills_scores(c, preferences["skills"]),
+                             self.get_dislikes_scores(c, pref_w["dislikes"]),
+                             self.get_likes_scores(c, pref_w["likes"]),
+                             self.get_friends_scores(c, pref_w["friends"])]
 
             score_per_course.append(sum(course_scores))
 
@@ -155,47 +167,40 @@ class Agent():
         for p in packages:
             package_score_list.append((p, self.calculate_score(p, pref_weight) / len(p)))
 
-        package_score_list.sort(key = lambda t: t[1])
+        package_score_list.sort(key=lambda t: t[1])
 
-        Agent.print_debug("Best packeges: ", package_score_list[-5:])
+        self.print_debug("Best packages: ", package_score_list[-5:])
         return package_score_list
 
-    def extract_topics(self, course_list):
-        return set([topic.name
-                    for topic in list(itertools.chain.from_iterable([c.covers for c in course_list]))
-                    ])
-
-    def similar_rank(self, taken, packages):
+    def similarity_rank(self, packages_and_rank_score):
         similar_score_list = []
-        course_packages = [t[0] for t in packages]
-        taken_topics = self.extract_topics(taken)
-        for idx, package in enumerate(packages):
+        course_packages = [t[0] for t in packages_and_rank_score]
+        taken_topics = self.extract_topics(self.extract_courses_taken())
+        for idx, package in enumerate(packages_and_rank_score):
             pack_topics = self.extract_topics(course_packages[idx])
-            similar_score_list.append((course_packages[idx], len(taken_topics.intersection(pack_topics)) / len(package[0]) + package[1]))
+            similar_score_list.append(
+                (course_packages[idx], len(taken_topics.intersection(pack_topics)) + package[1]))  # / len(package[0])
 
-        similar_score_list.sort(key = lambda t: t[1])
+        similar_score_list.sort(key=lambda _t: _t[1])
+        self.print_debug("Best after similarity ranking packages: ", similar_score_list[-5:])
         return similar_score_list
 
-
     def match_preferences(self):
-
         preferences = self.data["preferences"]
-        importance_list = self.data["importance"]
         period = preferences["period"]
         courses = self.get_courses_per_period()[period]
-        Agent.print_debug("Courses from period [" + period + "]: ", courses)
+        self.print_debug("Courses from period [" + period + "]: ", courses)
 
         packages = self.generate_combinations(courses)
-        Agent.print_debug("Generated Packages: ", packages, "Number: ", len(packages))
+        self.print_debug("Generated Packages: ", packages, "Number: ", len(packages))
         ranked_packages = self.rank(packages)
         # if the best packages have the same score
         if ranked_packages[-1][1] == ranked_packages[-2][1]:
-            similar_packages = self.similar_rank(self.extract_courses_taken(), ranked_packages)
-
+            similar_packages = self.similarity_rank(ranked_packages)
+            return similar_packages[-1]
 
         return ranked_packages[-1]
 
-    # checked
     def get_courses_per_period(self):
         taken_course = self.extract_courses_taken()
         possible_course = self.extract_possible_courses()
@@ -203,8 +208,8 @@ class Agent():
                                             if len(c.hasPreliminary) <= 0
                                             ]
 
-        takeble = set(possible_course).union(set(basic_course_without_preliminary))
-        final_takeble = takeble.difference(set(taken_course))
+        takeable = set(possible_course).union(set(basic_course_without_preliminary))
+        final_takeable = takeable.difference(set(taken_course))
 
         course_per_periods = {"P1": [],
                               "P2": [],
@@ -212,7 +217,7 @@ class Agent():
                               "P4": []
                               }
 
-        for course in final_takeble:
+        for course in final_takeable:
             period = str(course.isTaughtOnPeriod[0]).split(".")[1]
             if period == "P1":
                 course_per_periods["P1"].append(course)
@@ -225,14 +230,13 @@ class Agent():
 
         return course_per_periods
 
-    # checked
     def get_all_courses(self):
 
-        HumanitiesCourses    = list(self.ontology.HumanitiesCourse.instances())
-        ScienceCourses       = list(self.ontology.ScienceCourse.instances())
-        SocialCourses        = list(self.ontology.SocialCourse.instances())
+        humanities_courses = list(self.ontology.HumanitiesCourse.instances())
+        science_courses = list(self.ontology.ScienceCourse.instances())
+        social_courses = list(self.ontology.SocialCourse.instances())
 
-        return HumanitiesCourses + ScienceCourses + SocialCourses
+        return humanities_courses + science_courses + social_courses
 
     def check_hobbies(self):
         with self.ontology:
@@ -242,25 +246,22 @@ class Agent():
                 try:
                     sync_reasoner(infer_property_values=True)
                 except OwlReadyInconsistentOntologyError:
-                    print("Inconsistent")
+                    self.print_debug("INCONSISTENCY: Student can't take course on same day as hobby.")
 
 
 def course_planning(agent):
-
-    coursesTaken        = agent.extract_courses_taken()
-    all_courses         = agent.get_all_courses()
-    most_similar        = agent.get_similar_courses_to(coursesTaken[0], all_courses, 3)
-    Agent.print_debug("Taken Courses: ", coursesTaken)
-    Agent.print_debug("Most similar courses to: [", coursesTaken[0], "] are the following: ", most_similar)
+    courses_taken = agent.extract_courses_taken()
+    all_courses = agent.get_all_courses()
+    most_similar = agent.get_similar_courses_to(courses_taken[0], all_courses, 3)
+    agent.print_debug("Taken Courses: ", courses_taken)
+    agent.print_debug("Most similar courses to: [", courses_taken[0], "] are the following: ", most_similar)
 
     agent.check_hobbies()
     agent.match_preferences()
 
 
-
 def main():
-
-    onto_agent = Agent(DATA_FILE, 0)
+    onto_agent = Agent(DATA_FILE)
     course_planning(onto_agent)
 
 
